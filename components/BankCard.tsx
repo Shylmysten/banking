f966b7706b5c6caf758ee6d277bc830b20652670
Tony Boswell <tboswell@imodules.com>
2024-09-13 15:40:14 -0500

    Finalize the app

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,18 +2,19 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
         <Link
-            href='/'
+            href={`/transaction-history/?id=${account.appwriteItemId}`}
             className='bank-card'
         >
             <div className="bank-card_content">
                 <div>
                     <h1 className="text-16 font-semibold text-white">
-                        {userName }
+                        {account.name }
                     </h1>
                     <p className="font-ibm-plex-serif font-black text-white">
                         {formatAmount(account.currentBalance)}
@@ -30,7 +31,9 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                         </h2>
                     </div>
                     <p className="text-14 font-semibold tracking-[1.1px] text-white">
-                    ◍◍◍◍ ◍◍◍◍ ◍◍◍◍ <span className='text-16'>1234</span>
+                    ◍◍◍◍ ◍◍◍◍ ◍◍◍◍ <span className='text-16'>
+                        {account?.mask}
+                    </span>
                     </p>
                 </article>
             </div>
@@ -61,7 +64,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
             />
         </Link>
 
-        {/* COPY */}
+        {showBalance && <Copy title={account?.shareableId}/>}
     </div>
   )
 }
